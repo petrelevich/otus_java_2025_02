ec131e3404855772c6e61db2e37dacbe79e3b92c
stvort <stvort@mail.ru>
2025-06-10 22:43:52 +0400

    L25-di updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,8 @@ allprojects {
 	val jetty: String by project
     val freemarker: String by project
 
+    val reflections: String by project
+
     apply(plugin = "io.spring.dependency-management")
     dependencyManagement {
         dependencies {
@@ -67,6 +69,8 @@ allprojects {
             dependency("org.eclipse.jetty:jetty-io:$jetty")
             dependency("org.eclipse.jetty:jetty-util:$jetty")
             dependency("org.freemarker:freemarker:$freemarker")
+
+            dependency("org.reflections:reflections:$reflections")
         }
     }
 
